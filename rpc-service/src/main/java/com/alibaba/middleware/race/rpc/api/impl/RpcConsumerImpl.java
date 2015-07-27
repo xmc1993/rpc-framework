a5e7f7cf67e9e67eff2ab0bfd69f2dab6e9f9b1f
xmc1993 <1170143845@qq.com>
2015-07-28 00:20:29 +0800

    添加基本类

--- rpc-service/src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcConsumerImpl.java
+++ rpc-service/src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcConsumerImpl.java
@@ -1,8 +1,10 @@
 package com.alibaba.middleware.race.rpc.api.impl;
 
 
+import com.alibaba.middleware.race.rpc.api.RpcConsumer;
+
 /**
  * Created by xmc1993 on 2015/7/27.
  */
-public class RpcConsumerImpl{
+public class RpcConsumerImpl extends RpcConsumer{
 }
--- rpc-service/src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcProviderImpl.java
+++ rpc-service/src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcProviderImpl.java
@@ -1,7 +1,9 @@
 package com.alibaba.middleware.race.rpc.api.impl;
 
+import com.alibaba.middleware.race.rpc.api.RpcProvider;
+
 /**
  * Created by xmc1993 on 2015/7/27.
  */
-public class RpcProviderImpl {
+public class RpcProviderImpl extends RpcProvider {
 }
