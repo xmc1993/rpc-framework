cd5fc07d9c582310ecb39feabe1c49786ea5f8c6
xmc1993 <1170143845@qq.com>
2015-07-28 00:23:53 +0800

    添加实现类
    添加实现类

---
+++ rpc-service/src/main/java/com/alibaba/middleware/race/rpc/aop/impl/ProviderHookImpl.java
@@ -0,0 +1,17 @@
+package com.alibaba.middleware.race.rpc.aop.impl;
+
+import com.alibaba.middleware.race.rpc.aop.ProviderHook;
+import com.alibaba.middleware.race.rpc.model.RpcRequest;
+
+/**
+ * Created by xmc1993 on 2015/7/28.
+ */
+public class ProviderHookImpl implements ProviderHook{
+    public void before(RpcRequest rpcRequest) {
+
+    }
+
+    public void after(RpcRequest rpcRequest) {
+
+    }
+}
